562d8777308c11f687088687ed6019ad03e4746b
Razvan Turturica <razvan.turtu@yahoo.com>
2022-11-27 21:56:43 +0200

    Frontend initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const AppContainer = styled.div``;
+
+const App = () => {
+    return <AppContainer></AppContainer>;
+};
+
+export default App;
